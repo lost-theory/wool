950ac1f79fd20b0eea6cc4e1ce87a8141711f0e7
Steven Kryskalla <skryskalla@gmail.com>
2025-03-02 14:18:03 -0500

    uninfra: refactoring, add User resource, argparse

--- uninfra/uninfra.py
+++ uninfra/uninfra.py
@@ -1,31 +1,13 @@
 """
-Experimental CM code for consolidation.
+Uninfra: Pure Python Configuration Management.
 """
 
 import argparse
 import os
 import subprocess
 from string import Template
-from subprocess import check_call, check_output
 
-## start nginx code ###########################################################
-
-NGINX_CONFIG = """
-server {
-  listen 0.0.0.0:80;
-  server_name 0.0.0.0 ${ips};
-
-  location / {
-    proxy_pass http://app;
-  }
-}
-
-upstream app {
-  server unix:/tmp/gunicorn.sock;
-}
-"""
-
-# code
+# XXX: some code I once used for writing Nginx configs using a `Template`, with "idempotency check"
 """
 ips = check_output(["hostname", "--all-ip-addresses"]).strip()
 with open("/tmp/app.conf", "w") as f:
@@ -37,9 +19,9 @@ if file_needs_update(src="/tmp/app.conf", dst="/etc/nginx/conf.d/app.conf"):
     run(["sudo", "service", "nginx", "reload"])
 """
 
-## end nginx code #############################################################
-
 ## start systemd code #########################################################
+
+# XXX: currently unused
 
 SERVICE_CONFIG = """
 [Unit]
@@ -80,17 +62,17 @@ if file_needs_update(src=gunicorn_service_src, dst=gunicorn_service_dst):
 
 def run(cmd, **kw):
     print("Running: {} with {}".format(cmd, kw))
-    check_call(cmd, **kw)
+    subprocess.check_call(cmd, **kw)
 
 
 def run_with_output(cmd, **kw):
     print("Getting output from: {} with {}".format(cmd, kw))
-    output = check_output(cmd, **kw).decode("utf8")
-    return output
+    result = subprocess.run(cmd, **kw, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    return (result.returncode, result.stdout, result.stderr)
 
 
 def checksum(path):
-    checksum, _ = check_output(["sha256sum", path]).decode("utf8").strip().split()
+    checksum, _ = subprocess.check_output(["sha256sum", path], text=True).strip().split()
     return checksum
 
 
@@ -113,6 +95,53 @@ class Directory:
         run(["mkdir", "-p", self.path])
 
 
+class User:
+    def __init__(self, username, groups=None, shell=None, home=None, system=False):
+        self.username = username
+        self.wanted_groups = groups or []
+        self.shell = shell
+        self.home = home
+        self.system = system
+
+    def get_current_groups(self):
+        """Get list of groups user belongs to."""
+        (status, out, err) = run_with_output(["groups", self.username])
+        if status == 0:
+            return out.split(":")[1].strip().split()
+        else:
+            raise RuntimeError(f"Got unexpected response from `groups`: {(status, out, err)!r}.")
+
+    def run(self):
+        # Check if user exists
+        (status, out, err) = run_with_output(["id", self.username])
+        user_exists = status == 0 and "uid=" in out
+
+        if not user_exists:
+            cmd = ["sudo", "useradd"]
+            if self.system:
+                cmd.append("--system")
+            if self.shell:
+                cmd.extend(["--shell", self.shell])
+            if self.home:
+                cmd.extend(["--home-dir", self.home])
+            if self.wanted_groups:
+                cmd.extend(["--groups", ",".join(self.wanted_groups)])
+            cmd.append(self.username)
+            run(cmd)
+        else:
+            print(f"Skipping user creation for {self.username!r} because user already exists.")
+
+        if self.wanted_groups:
+            current_groups = self.get_current_groups()
+            groups_to_add = set(self.wanted_groups) - set(current_groups)
+            groups_to_remove = set(current_groups) - set(self.wanted_groups)
+
+            for group in groups_to_add:
+                run(["sudo", "usermod", "-a", "-G", group, self.username])
+            for group in groups_to_remove:
+                run(["sudo", "gpasswd", "-d", self.username, group])
+
+
 class Download:
     def __init__(self, url, provides):
         self.url = url
@@ -126,15 +155,34 @@ class Download:
 
 
 class AptPackage:
-    def __init__(self, name, provides):
+    def __init__(self, name, provides=None):
         self.name = name
-        self.provides = os.path.expanduser(provides)
+        self.provides = os.path.expanduser(provides) if provides else None
+
+    def is_installed(self):
+        (_, output, _) = run_with_output(["dpkg-query", "-W", "-f='${Status}'", self.name])
+        if "ok not-installed" in output:
+            return False
+        elif "ok installed" in output:
+            return True
+        else:
+            raise RuntimeError(f"Unable to parse dkpg-query result: {output!r}.")
 
     def run(self):
-        if os.path.isfile(self.provides):
-            print(f"Skipping apt install of {self.name} because {self.provides} exists.")
-            return
-        run(["sudo", "apt-get", "install", "-y", self.name])
+        install = False
+        if self.provides:
+            if os.path.isfile(self.provides):
+                print(f"Skipping apt install of {self.name} because {self.provides} exists.")
+            else:
+                install = True
+        else:
+            if self.is_installed():
+                print(f"Skipping apt install of {self.name} because it's already installed.")
+            else:
+                install = True
+
+        if install:
+            run(["sudo", "apt-get", "install", "-y", self.name])
 
 
 class Virtualenv:
@@ -166,55 +214,66 @@ class Command:
 ## main #######################################################################
 
 
-def cm_run():
+def demo():
     steps = [
         AptPackage("nginx", provides="/usr/sbin/nginx"),
         Directory("/tmp/foo/bar"),
-        Download("http://lost-theory.org/robots.txt", provides="/tmp/robots.txt"),
+        Download("http://lost-theory.org/robots.txt", provides="/tmp/foo/bar/robots.txt"),
         Virtualenv("/usr/bin/python3", "~/testing-env"),
-        Command("/bin/bash", "-c", "date | tee -a /tmp/out.log"),
+        Command("/bin/bash", "-c", "date | tee -a /tmp/foo/out.log"),
     ]
     for step in steps:
         step.run()
 
 
-def push(remote):
+def ltorg_stage_1():
+    steps = [
+        AptPackage("mercurial", provides="/usr/bin/hg"),
+        AptPackage("python3.12-venv"),
+        User("app"),
+        Directory("/tmp/foo/bar"),
+        Download("http://lost-theory.org/robots.txt", provides="/tmp/foo/bar/robots.txt"),
+        Virtualenv("/usr/bin/python3", "~/testing-env"),
+        Command("/bin/bash", "-c", "date | tee -a /tmp/foo/out.log"),
+    ]
+    for step in steps:
+        step.run()
+
+
+def cm_run(task_name):
+    tasks = {
+        "demo": demo,
+        "ltorg_stage_1": ltorg_stage_1,
+    }
+    task_func = tasks[task_name]
+    return task_func()
+
+
+def push(remote, task_name):
     # Remote python version check
-    version_check = run_with_output(
-        [
-            "ssh",
-            "-A",
-            remote,
-            "python3 -c 'import platform; print(tuple(map(int, platform.python_version_tuple())) >= (3, 6, 0))'",
-        ]
-    )
-    assert version_check.strip() == "True", "Invalid remote python version. Need >=3.6.0."
+    (_, output, _) = run_with_output(["ssh", "-A", remote, "python3 -c 'import platform; print(tuple(map(int, platform.python_version_tuple())) >= (3, 6, 0))'"])
+    assert output.strip() == "True", "Invalid remote python version. Need >=3.6.0."
 
     # Push and run
     run(["scp", __file__, "{}:/tmp/cm.py".format(remote)])
-    run(["ssh", "-A", remote, "python3 -u /tmp/cm.py --run"])
+    run(["ssh", "-A", remote, f"python3 -u /tmp/cm.py --run --task={task_name}"])
 
 
 def main():
     parser = argparse.ArgumentParser(description="Simple pure python config management")
-    parser.add_argument(
-        "--push",
-        type=str,
-        metavar="USER@HOST",
-        help="Push and run on remote user@host via SSH.",
-    )
-    parser.add_argument(
-        "--run",
-        action="store_true",
-        help="Used internally by --push. Or you can run it yourself on a local machine.",
-    )
+    parser.add_argument("--push", type=str, metavar="USER@HOST", help="Push and run on remote user@host via SSH.")
+    parser.add_argument("--run", action="store_true", help="Used internally by --push. Or you can run it yourself on a local machine.")
+    parser.add_argument("--task", type=str, metavar="NAME", help="The name of the task to run.")
 
     args = parser.parse_args()
 
-    if args.push:
-        push(args.push)
-    elif args.run:
-        cm_run()
+    if args.task:
+        assert args.task.isidentifier(), f"Invalid task name: {args.task!r}"
+
+    if args.push and args.task:
+        push(args.push, args.task)
+    elif args.run and args.task:
+        cm_run(args.task)
     else:
         parser.print_help()
 
